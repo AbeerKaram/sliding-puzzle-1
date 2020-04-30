a750ed56058663472f5ba4127002eddaee520cde
AbeerKaram <abeerkhalid571@gmail.com>
2020-04-30 10:00:08 +0300

    Render the message if the gamer wins #9

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Game from './componants/Game';
-import Result from './componants/Result';
 
 function App() {
   return (
     <div className="App">
       <Game />
-      <Result />
     </div>
   );
 }
